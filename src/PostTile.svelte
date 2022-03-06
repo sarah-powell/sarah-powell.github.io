f1a66a754d46c8eecde950c09db095a5d562b1e6
sarah-powell <sarahpowell149@gmail.com>
2022-03-06 16:39:24 -0500

    Added post tile

---
+++ src/PostTile.svelte
@@ -0,0 +1,41 @@
+<script>
+    export let title = "Title"
+    export let description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
+    export let date = "Jan 1, 1970"
+</script>
+
+
+<div class="Tile">
+    <div class="title">{title}</div>
+    <div class="description">{description}</div>
+    <div class="date">{date}</div>
+</div>
+
+<style>
+
+    .Tile {
+        background-color: #395E43;
+        color: white;
+        display: flex;
+        margin: 8px;
+        padding: 8px;
+        border-radius: 10px;
+        
+        width: 120px;
+        min-height: 70px;
+
+        flex-direction: column;
+        justify-content: space-between;
+        row-gap: 8px;
+    }
+    
+    .description {
+        font-size: 12px;
+    }
+
+    .date {
+        font-style: italic;
+        font-size: 14px;
+    }
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,21 @@
 <script>
+    import { each } from "svelte/internal";
+    import PostTile from "/src/PostTile.svelte";
+
     import "../app.css";
+
+    let tiles = [
+        {
+            "title": "First post",
+            "description": "Lorem ipsum dolor sit amet, consectetur...",
+            "date": "Mar 6, 2022"
+        },
+        {
+            "title": "Second post",
+            "description": "Lorem ipsum dolor sit ",
+            "date": "Jan 18, 2038"
+        }
+    ]
 </script>
 
 <svelte:head>
@@ -7,11 +23,41 @@
 </svelte:head>
 
 <div class="content">
-    Main content well
+    {#each tiles as tile}
+        <PostTile {...tile}/>
+        <PostTile {...tile}/>
+        <PostTile {...tile}/>
+        <PostTile {...tile}/>
+        <PostTile {...tile}/>
+        <PostTile {...tile}/>
+        <PostTile {...tile}/>
+        <PostTile {...tile}/>
+        <PostTile {...tile}/>
+        <PostTile {...tile}/>
+    {/each}
 </div>
 
 <style>
     
-    
-        
+    .content {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        flex-grow: 1;
+        justify-content: center;
+
+        margin: 20px 16px;
+    }
+
+    @media only screen and (min-width: 500px) {
+        .content {
+            margin: 20px 64px;
+        }
+    }
+     
+    @media only screen and (min-width: 800px) {
+        .content {
+            margin: 20px 256px;
+        }
+    }
 </style>
